a4cadfc0bffc1aa7cf8e36536b8385e948ba4d5f
Bill Fraser <bill@wfraser.dev>
2024-05-29 13:31:30 -0700

    args

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,30 @@ use clap_wrapper::clap_wrapper;
 /// DBX CLI
 #[clap_wrapper]
 #[derive(Debug, Parser)]
-#[clap(version)]
+#[command(version)]
 struct Args {
+    #[command(subcommand)]
+    op: Operation,
+
+    #[command(flatten)]
+    common: CommonOptions,
+}
+
+/// Common options
+#[clap_wrapper]
+#[derive(Debug, Parser)]
+struct CommonOptions {
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+#[derive(Debug, Clone, Parser)]
+enum Operation {
+    /// Check the state of the local filesystem.
+    Check,
+
+    /// Pull updates from the server and update the local filesystem.
+    Pull,
 }
 
 fn main() -> anyhow::Result<()> {
