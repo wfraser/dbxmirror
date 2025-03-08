683c09edbe08b2569680992adcd3d7cef4d44a01
Bill Fraser <bill@wfraser.dev>
2025-03-07 18:38:05 -0800

    ops: don't end up with folder add followed immediately by delete
    Dropbox tends to add a folder by issuing a delete for it followed by an add; the current list_folder_to_ops will invert the order of those, which is not great :)

--- src/ops.rs
+++ src/ops.rs
@@ -2,7 +2,7 @@ use crate::db::Database;
 use crate::StrExt;
 use anyhow::anyhow;
 use dropbox_sdk::files::{FileMetadata, Metadata};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub enum Op {
     AddedFile(String, Box<FileMetadata>),
@@ -26,87 +26,102 @@ pub fn list_folder_to_ops(
         "remote_root must end in a slash"
     );
     let mut adds = HashMap::<String, Vec<(String, FileMetadata)>>::new();
-    let mut folders = vec![];
-    let mut deleted = vec![];
 
-    'entries: for entry in entries {
-        let path = match &entry {
-            Metadata::File(f) => f.path_display.as_ref(),
-            Metadata::Deleted(d) => d.path_display.as_ref(),
-            Metadata::Folder(d) => d.path_display.as_ref(),
-        };
-        if path
-            .map(|s| s.eq_ignore_case(&remote_root[..remote_root.len() - 1]))
-            .unwrap_or(false)
-        {
-            // This is an entry for the root itself.
-            continue;
-        }
-        let path = path
-            .ok_or_else(|| anyhow!("missing path_display field for Metadata entry"))?
-            .strip_prefix_ignore_case(remote_root)
-            .ok_or_else(|| {
-                anyhow!(
-                    "remote path {:?} doesn't start with root path {:?}",
-                    path,
-                    remote_root,
-                )
-            })?
-            .to_owned();
-
-        for (test, is_regex) in ignores {
-            if *is_regex {
-                todo!("regex support")
-            } else if path.starts_with(test) {
-                continue 'entries;
+    // First, filter out ignored paths, then find all added files and map them by content hash.
+    let entries = entries
+        .into_iter()
+        .filter_map(|entry| -> Option<anyhow::Result<_>> {
+            debug!("entry: {entry:?}");
+            let path = match &entry {
+                Metadata::File(f) => f.path_display.as_ref(),
+                Metadata::Deleted(d) => d.path_display.as_ref(),
+                Metadata::Folder(d) => d.path_display.as_ref(),
+            };
+            if path
+                .map(|s| s.eq_ignore_case(&remote_root[..remote_root.len() - 1]))
+                .unwrap_or(false)
+            {
+                // This is an entry for the root itself.
+                return None;
             }
-        }
 
+            let path = match path {
+                Some(p) => p,
+                None => {
+                    return Some(Err(anyhow!(
+                        "missing path_display field for Metadata entry"
+                    )))
+                }
+            };
+            let path = match path.strip_prefix_ignore_case(remote_root) {
+                Some(p) => p.to_owned(),
+                None => {
+                    return Some(Err(anyhow!(
+                        "remote path {:?} doesn't start with root path {:?}",
+                        path,
+                        remote_root,
+                    )))
+                }
+            };
+
+            for (test, is_regex) in ignores {
+                if *is_regex {
+                    todo!("regex support")
+                } else if path.starts_with(test) {
+                    return None;
+                }
+            }
+
+            if let Metadata::File(meta) = &entry {
+                let hash = match meta.content_hash.as_ref() {
+                    Some(h) => h.to_owned(),
+                    None => {
+                        return Some(Err(anyhow!(
+                            "missing content hash from server for {path:?}"
+                        )))
+                    }
+                };
+                adds.entry(hash)
+                    .or_default()
+                    .push((path.clone(), meta.clone()));
+            }
+
+            Some(Ok((path, entry)))
+        })
+        .collect::<anyhow::Result<Vec<(String, _)>>>()?;
+
+    let mut move_dests = HashSet::new();
+
+    let mut ops = vec![];
+    for (path, entry) in entries {
         match entry {
             Metadata::File(meta) => {
-                let hash = meta
-                    .content_hash
-                    .clone()
-                    .ok_or_else(|| anyhow!("missing content hash from server for {path:?}"))?;
-                adds.entry(hash).or_default().push((path, meta));
+                if move_dests.remove(&path) {
+                    // already did a move for this path; no need for an add
+                    continue;
+                }
+                ops.push(Op::AddedFile(path, Box::new(meta)));
             }
-            Metadata::Folder(_) => folders.push(path),
-            Metadata::Deleted(_) => deleted.push(path),
-        }
-    } // entries
-
-    // Start by creating all folders.
-    let mut ops = folders
-        .into_iter()
-        .map(Op::CreateFolder)
-        .collect::<Vec<Op>>();
-
-    // Look for matching adds for each delete and turn them into moves.
-    for deleted_path in deleted {
-        let Some((_mtime, hash)) = db.get_file(&deleted_path)? else {
-            // Unknown file deted. This is probably a problem but let the caller deal with it.
-            ops.push(Op::DeletedFile(deleted_path));
-            continue;
-        };
-
-        if let Some(files) = adds.get_mut(&hash) {
-            let (added_path, meta) = files.pop().unwrap(); // not allowed to be empty
-            ops.push(Op::MovedFile {
-                old_path: deleted_path,
-                new_path: added_path,
-                remote: Box::new(meta),
-            });
-            if files.is_empty() {
-                adds.remove(&hash);
+            Metadata::Folder(_) => ops.push(Op::CreateFolder(path)),
+            Metadata::Deleted(_) => {
+                if let Some((hash, files)) = db
+                    .get_file(&path)?
+                    .and_then(|(_mtime, hash)| adds.get_mut(&hash).map(|files| (hash, files)))
+                {
+                    let (added_path, meta) = files.pop().unwrap(); // not allowed to be empty
+                    ops.push(Op::MovedFile {
+                        old_path: path,
+                        new_path: added_path.clone(),
+                        remote: Box::new(meta),
+                    });
+                    move_dests.insert(added_path);
+                    if files.is_empty() {
+                        adds.remove(&hash);
+                    }
+                } else {
+                    ops.push(Op::DeletedFile(path));
+                }
             }
-        } else {
-            ops.push(Op::DeletedFile(deleted_path));
-        }
-    }
-
-    for (_hash, files) in adds {
-        for (path, meta) in files {
-            ops.push(Op::AddedFile(path, Box::new(meta)));
         }
     }
 
